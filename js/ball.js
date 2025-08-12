e84038dc38128d207168304eebd611834d72cc42
riznykalex <riznyk.alex.mail@gmail.com>
2025-08-12 10:43:18 +0300

    Загальні правки

--- js/ball.js
+++ js/ball.js
@@ -1,43 +1,59 @@
-//ball.js
+// ball.js
 export class Ball {
-  constructor(id, x, y, power, game, width, height) {
-    this.id = id;
-    this.x = x;
-    this.y = y;
-    this.power = power;
-    this.size = 30 + power * 10;
-    this.speed = 2;
-    this.isMoving = false;
-    this.isControlled = false;
-    this.isSleeping = false;
-    this.vx = 0;
-    this.vy = 0;
-    this.game = game;
-    this.width = width;
-    this.height = height;
+    constructor(id, x, y, power, gameElement, width, height) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.power = power; // наприклад, впливає на швидкість або розмір
+        this.gameElement = gameElement;
+        this.width = width || 30;
+        this.height = height || 30;
+        this.speedX = 0;
+        this.speedY = 0;
 
-    this.div = document.createElement('div');
-    this.div.classList.add('ball');
-    this.div.style.position = 'absolute';
-    this.updatePosition();
-    this.game.appendChild(this.div);
-  }
+        // Створення HTML-елемента
+        this.element = document.createElement("div");
+        this.element.className = "ball";
+        this.element.style.position = "absolute";
+        this.element.style.width = this.width + "px";
+        this.element.style.height = this.height + "px";
+        this.element.style.borderRadius = "50%";
+        this.element.style.background = this.getRandomColor();
 
-  move(vx, vy) {
-    this.x += vx;
-    this.y += vy;
+        // Додаємо на поле
+        this.gameElement.appendChild(this.element);
 
-    // обмеження по межах вікна
-    this.x = Math.max(0, Math.min(this.x, this.width - this.size));
-    this.y = Math.max(0, Math.min(this.y, this.height - this.size));
+        // Початкове відображення
+        this.render();
+    }
 
-    this.updatePosition();
-  }
+    // Випадковий колір
+    getRandomColor() {
+        const colors = ["red", "blue", "green", "orange", "purple", "pink"];
+        return colors[Math.floor(Math.random() * colors.length)];
+    }
 
-  updatePosition() {
-    this.div.style.width = this.size + 'px';
-    this.div.style.height = this.size + 'px';
-    this.div.style.left = this.x + 'px';
-    this.div.style.top = this.y + 'px';
-  }
+    // Відображення позиції
+    render() {
+        this.element.style.left = this.x + "px";
+        this.element.style.top = this.y + "px";
+    }
+
+    // Оновлення руху
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        this.render();
+    }
+
+    // Задати напрямок руху
+    setDirection(dx, dy) {
+        this.speedX = dx;
+        this.speedY = dy;
+    }
+
+    // Видалення з гри
+    remove() {
+        this.element.remove();
+    }
 }
