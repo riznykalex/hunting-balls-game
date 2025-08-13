caa79d492085d7a66aaf50602dc161535b2d01e3
riznykalex <riznyk.alex.mail@gmail.com>
2025-08-13 19:53:56 +0300

    нова версія

--- js/food.js
+++ js/food.js
@@ -1,15 +1,16 @@
-export default class Food {
-    constructor(x, y, size = 5, color = 'green') {
-        this.x = x;
-        this.y = y;
-        this.size = size;
-        this.color = color;
-    }
+// food.js
+export class Food {
+  constructor(x, y, radius = 5, color = 'green') {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  }
 
-    render(ctx) {
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-        ctx.fillStyle = this.color;
-        ctx.fill();
-    }
+  render(ctx) {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fill();
+  }
 }
